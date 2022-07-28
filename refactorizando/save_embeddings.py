b696f4e6f6bf34e01d2f5469b9eb4685a8156d01
Magali Boulanger <maga.boulanger8@gmail.com>
2022-07-28 16:51:44 -0300

    archivo save_embeddings.py agregado

---
+++ refactorizando/save_embeddings.py
@@ -0,0 +1,22 @@
+from helper_2 import Helper
+from text_processing import TextProcessing
+
+tp = TextProcessing()
+helper = Helper(tp)
+texts = helper.get_texts_of_json('train')
+
+w, h = 2, 8500
+output = [[0 for x in range(w)] for y in range(h)]
+i=0
+for text in texts:
+    output[i][0]=text
+    output[i][1]=tp.encode_sentences(text)
+    i+=1
+
+f = open ('results/output_embeddings.txt','w')
+f.write(output)
+f.close()
+
+g = open ('results/output_texts.txt','w')
+g.write(texts)
+g.close()
